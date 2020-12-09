6b74a96c10a79f6fe5a84eca4c879fa67d3a15b1
geese.howard <saad15100097@outlook.com>
2020-12-09 02:43:26 +0000

    Project 1

--- scripts/common.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    $("head").prepend("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-    $("head").prepend("<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css\">");
-    $("head").prepend("<link rel=\"stylesheet\" href=\"./css/common.css\">");
-    $("body").prepend("<div id=\"navbar-placeholder\"></div>");
-    $("#navbar-placeholder").load("./navbar.html");
-});
